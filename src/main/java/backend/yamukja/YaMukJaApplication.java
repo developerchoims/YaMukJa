97f5a78d90600056708b79722aaab3f6d6365cf9
Chae Min Lim <coals722@naver.com>
2024-08-27 22:01:27 +0900

    project base setup

---
+++ src/main/java/backend/yamukja/YaMukJaApplication.java
@@ -0,0 +1,13 @@
+package backend.yamukja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaMukJaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaMukJaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/yamukja/YaMukJaApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.yamukja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaMukJaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
