35ff4851471ad6ffc26b3f81d4d7ccb71f022024
yujin <uj0791navercom@skuniv.ac.kr>
2024-08-28 18:38:38 +0900

    [Feat] 유저 entity 생성
    Hibernate Spatial 라이브러리 추가
    - Hibernate 버전 확인 후 적용
    - MySql의 GEOMETRY 타입으로 위치 저장

---
+++ src/main/java/backend/yamukja/user/entity/User.java
@@ -0,0 +1,31 @@
+package backend.yamukja.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(columnDefinition = "GEOMETRY")
+    private Point geography;
+
+    @Column(nullable = false)
+    private Boolean isLunchRecommend = false;
+}
